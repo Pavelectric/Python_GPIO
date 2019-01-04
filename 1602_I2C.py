dde10e0ad085aafa58b68065e4fd4b8f2a5a6cd8
Pavel <46323877+Pavelectric@users.noreply.github.com>
2019-01-04 17:21:10 +0300

    Date and Time

--- 1602_I2C.py
+++ 1602_I2C.py
@@ -2,7 +2,7 @@
 # The display 1602 works on the I2C bus, it has the address 3f.                     #
 # The device address can be viewed by executing the command "sudo i2cdetect-y 2"    #
 # You will see your current time and date on the display.                           #
-# autor - Pavel (Pavelectric) pavelectric@mail.ru                                   #
+# Autor - Pavel (Pavelectric) pavelectric@mail.ru                                   #
 #####################################################################################
 
 import smbus
