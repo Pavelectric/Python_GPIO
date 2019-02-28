193ded90b22d2a2302c7e2b3729ea461e65a727f
Pavel <46323877+Pavelectric@users.noreply.github.com>
2019-02-28 18:02:40 +0300

    Update oled_128x64_I2C+280.py

--- oled_128x64_I2C+280.py
+++ oled_128x64_I2C+280.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#####################################################################################
+# The oled display 128x64 works on the I2C bus, it has the address 3c               #
+# The following pins are involved - 2(VCC), 6(GND), 3(SDA), 5(SCL)                  #
+# Bme280 it has the address 0x76                                                    #
+# The device address can be viewed by executing the command "sudo i2cdetect-y 2"    #
+# You will see your current time and date on the display.                           #
+# Autor - Pavel (Pavelectric) pavelectric@mail.ru                                   #
+#####################################################################################
+
 import bme280
 from device import ssd1306
 from render import canvas
